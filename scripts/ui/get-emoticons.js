420cb88eda4066cc3eeddb056823f00ca68908ed
silweto7 <silweto7@abv.bg>
2016-08-04 02:00:05 +0300

    input emoticons with drop-down menu

--- scripts/ui/get-emoticons.js
+++ scripts/ui/get-emoticons.js
@@ -1,1 +1,19 @@
-//TODO: implement emoticons
+/* When the user clicks on the button,
+ toggle between hiding and showing the dropdown content */
+function dropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('#emoticons-button')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var d = 0; d < dropdowns.length; d++) {
+            var openDropdown = dropdowns[d];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
