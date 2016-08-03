e4574102a43cbcff1cb1871f7fb7d894308b603c
silweto7 <silweto7@abv.bg>
2016-08-03 22:01:58 +0300

    set styles and add new shapes-star and line

---
+++ scripts/shapes/shape-factory/star.js
@@ -0,0 +1,19 @@
+function createStar() {
+    var star = new Kinetic.Star({
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2,
+        width: 100,
+        height: 100,
+        fill: currentColor,
+        draggable: true,
+        offset: {
+            x: 50,
+            y: 50
+        },
+        numPoints: 5,
+        innerRadius: 70,
+        outerRadius: 70,
+        stroke: 'black',
+        strokeWidth: 4
+});
+}
