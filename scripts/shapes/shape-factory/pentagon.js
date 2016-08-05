7bb3f61946f4c37b30020552881761a1f08e8769
AntonioDimitrov7 <antonio.dimitrov@mail.bg>
2016-08-05 16:57:54 +0300

    Add files via upload

---
+++ scripts/shapes/shape-factory/pentagon.js
@@ -0,0 +1,17 @@
+function createpentagon() {
+    var pentagon=new Kinetic.RegularPolygon({
+		x: canvasOptions.width / 2,
+        y: canvasOptions.height / 2,
+        width: 50,
+        height: 50,
+		sides:5,
+		radius:80,
+		fill: currentColor,
+        draggable: true,
+        offset: {
+            x: 50,
+            y: 50
+        }
+    });
+    return pentagon;
+}
