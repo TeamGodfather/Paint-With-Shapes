ee82b074be40e544f2c71f0a0d8613e695b9a4ce
MiglenaPetrova <megi_dreamer@abv.bg>
2016-08-07 18:38:28 -0700

    Added circle.js

---
+++ scripts/shapes/shape-factory/circle.js
@@ -0,0 +1,14 @@
+function createCircle() {
+    var circle = new Kinetic.Circle({
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2,
+        radius: 50,
+        fill: currentColor,
+        draggable: true,
+        offset: {
+            x: 50,
+            y: 50
+          }
+    });
+    return circle;
+}
