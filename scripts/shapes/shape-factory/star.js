d5537735def19167fc1662749f8155a50df07ff4
ivailoAndonov <ivailoandonov.andonov@gmail.com>
2016-08-09 12:52:42 +0300

    shape-star

--- scripts/shapes/shape-factory/star.js
+++ scripts/shapes/shape-factory/star.js
@@ -1,21 +1,15 @@
 // TODO:
 function createStar() {
-    var star = new Kinetic.Star({
+  var star = new Kinetic.Line({
         x: window.innerWidth / 2,
         y: window.innerHeight / 2,
-        width: 100,
-        height: 100,
         fill: currentColor,
         draggable: true,
+        closed: true,
         offset: {
             x: 50,
-            y: 50
-        },
-        numPoints: 5,
-        innerRadius: 70,
-        outerRadius: 70,
-        stroke: 'black',
-        strokeWidth: 4
+            y: 170
+        }
     });
     return star;
 }
