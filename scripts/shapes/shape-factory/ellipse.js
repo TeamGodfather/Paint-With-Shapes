0254ab44eeb07a0540ab963774b56404042aa70e
shakuu <todorov.h@gmail.com>
2016-08-11 12:52:18 +0300

    fixed: ellipse

--- scripts/shapes/shape-factory/ellipse.js
+++ scripts/shapes/shape-factory/ellipse.js
@@ -1,15 +1,18 @@
-var oval = new Kinetic.Ellipse({
-        x: window.innerWidth / 2,
-        y: window.innerHeight / 2,
-        radius: {
-          x: 100,
-          y: 50
-        },
-        fill: currentColor,
-        draggable: true,
-        offset: {
-            x: 0,
-            y: 0
-          }
-    });
-    return ellipse;
+
+function createEllipse() {
+  var oval = new Kinetic.Ellipse({
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2,
+    radius: {
+      x: 100,
+      y: 50
+    },
+    fill: currentColor,
+    draggable: true,
+    offset: {
+      x: 0,
+      y: 0
+    }
+  });
+  return oval;
+}
