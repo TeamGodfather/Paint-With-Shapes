b6b0a8a6995b9a8ad631ca830538828230a13bcb
Todor Georgiev <todorgeorgiev96@gmail.com>
2016-08-12 02:16:27 +0300

    now hides when button is clicked again

--- scripts/colorpicker/get-color.js
+++ scripts/colorpicker/get-color.js
@@ -1,3 +1,19 @@
+var isActive = false;
+
+var btnToggleColor = document
+    .querySelector('#toggle-color')
+    .addEventListener('click', function () {
+        isActive = !isActive;
+        if(isActive){
+            getColor();
+        }
+        else {
+            $('canvas').remove();
+            $('#colorpicker').css('display', 'none');
+        }
+    });
+
+
 function getColor() {
     // // DELTE THIS WHEN COLOR PICKER IS WORKING
     // if (currentColor === '#cc00cc') {
@@ -6,16 +22,9 @@ function getColor() {
     //     currentColor = '#cc00cc';
     // }
 
-    var canvasPlace = document.getElementById('colorpicker');
-    canvasPlace.style.display = 'block';
-    var canvas = document.createElement('canvas');
-    canvas.style.display = 'block';
-    canvas.style.float = 'left';
-    var ctx = canvas.getContext('2d');
-    canvasPlace.appendChild(canvas);
-    canvas.width = 256;
-    canvas.height = 256;
-    canvas.style.border = '8px solid yellowgreen';
+
+
+
 
     function getMousePos(canvas, evt) {
         var rect = canvas.getBoundingClientRect();
@@ -31,11 +40,25 @@ function getColor() {
     }
 
     function init(imageObj) {
+        var canvasPlace = document.getElementById('colorpicker');
+        canvasPlace.style.display = 'block';
+        var canvas = document.createElement('canvas');
+        canvas.style.display = 'block';
+        canvas.style.float = 'left';
+        var ctx = canvas.getContext('2d');
+        canvasPlace.appendChild(canvas);
+        canvas.width = 256;
+        canvas.height = 256;
+        // canvas.style.border = '8px solid yellowgreen';
+
         var mouseDown = false;
 
-        // canvas.addEventListener('mouseout', function () {
-        //     canvas.remove();
-        // });
+        //
+        canvas.addEventListener('mouseout', function () {
+            isActive = false;
+            canvasPlace.style.display = 'none';
+            canvas.remove();
+        });
         ctx.strokeStyle = '#444';
         ctx.lineWidth = 2;
 
@@ -43,6 +66,7 @@ function getColor() {
             if(!(evt.target instanceof HTMLCanvasElement)){
                 return;
             }
+
             mouseDown = true;
             var color,
                 mousePos = getMousePos(canvas, evt);
@@ -65,6 +89,7 @@ function getColor() {
                 return;
             }
 
+            isActive = false;
             mouseDown = false;
             canvas.remove();
             canvasPlace.style.display = 'none';
@@ -90,7 +115,7 @@ function getColor() {
         ctx.drawImage(imageObj, 0, 0);
 
         // This changes shape coloer, since default is not balck.
-        // Should Shape Default color be black ( can change in global-varialbes.js, initialze currentColor to black/ whatever ) ? 
+        // Should Shape Default color be black ( can change in global-varialbes.js, initialze currentColor to black/ whatever ) ?
         // drawColor('black');
     }
 
@@ -101,4 +126,5 @@ function getColor() {
 
     imageObj.crossOrigin = '';
     imageObj.src = './images/colorpicker.png';
+
 }
