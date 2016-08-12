0fc3ac089831d19c27524bbd71218d9260b33728
Todor Georgiev <todorgeorgiev96@gmail.com>
2016-08-12 03:57:19 +0300

    update:save now works

--- scripts/ui/save-button.js
+++ scripts/ui/save-button.js
@@ -1,1 +1,18 @@
 //TODO: implement save-button
+var saveButton = document.getElementById('save');
+
+saveButton.onclick = function () {
+    var saveCanvasPic = document.querySelector('.kineticjs-content canvas');
+    var data = saveCanvasPic.toDataURL();
+
+    var a = document.createElement('a');
+    a.href = data;
+
+    var name = prompt("Please set a name", "Default Name");
+    a.download = name;
+    a.style.display = 'none';
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+
+};
